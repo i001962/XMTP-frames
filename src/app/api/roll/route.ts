dc6c75a29b2e57b844671c971ca8579d9339f25e
i001962@gmail.com <i001962@gmail.com>
2024-04-28 14:54:33 -0700

    p

--- src/app/api/roll/route.ts
+++ src/app/api/roll/route.ts
@@ -60,40 +60,23 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     );
   } else {
     const imageUrl = `${NEXT_PUBLIC_URL}/api/images/goodGuess`;
+    const urlStartOver = `${NEXT_PUBLIC_URL}/api/roll`;
     return new NextResponse(
       `<!DOCTYPE html>
       <html>
         <head>
-          <title>Echo Says:</title>
+          <title>Guess The Price Change</title>
           <meta property="og:title" content="Good guess" />
           <meta property="og:image" content="${imageUrl}" />
 
-        
-          <meta name= "of:version" content="vNext"/>
-          <meta name= "of:accepts:farcaster" content="vNext"/>
-          <meta name= "of:accepts:xmtp" content="2024-02-01"/>
-          <meta name= "of:post_url" content="${postUrl}" />
-          <meta name= "of:image" content="${imageUrl}" />
-
-          <meta name="of:button:1" content="source code" />
-          <meta name="of:button:1:action" content="link" />
-          <meta name="of:button:1:target" content="https://github.com/LeoFranklin015/XMTP-frames" />
-
-          <meta name="of:frame:button:1" content="source" />
-          <meta name="of:frame:button:1:action" content="post" />
-          <meta name="of:frame:button:1:target" content="${postUrl}" />
-      
-          <meta name="fc:frame:button:1" content="Start over" />
-          <meta name="fc:frame:button:1:action" content="post" />
-          <meta name="fc:frame:button:1:target" content="${postUrl}" />
+          <meta name="fc:frame" content="vNext" />
+          <meta name="fc:frame:post_url" content="${postUrl}" />
+          <meta name="fc:frame:image" content="${imageUrl}" />
           
-          <meta name="of:button:2" content="source code" />
-          <meta name="of:button:2:action" content="link" />
-          <meta name="of:button:2:target" content="https://github.com/LeoFranklin015/XMTP-frames" />
-
-          <meta name="fc:frame:button:2" content="source code" />
-          <meta name="fc:frame:button:2:action" content="link" />
-          <meta name="fc:frame:button:2:target" content="https://github.com/LeoFranklin015/XMTP-frames" />
+          <meta name="fc:frame:button:1" content="1" />
+          <meta name="fc:frame:button:1:action" content="post" />
+          <meta name="fc:frame:button:1:target" content="${urlStartOver}" />
+          
         </head>
         <body/>
       </html>`,
