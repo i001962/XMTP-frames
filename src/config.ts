65788dab99ef8f28d0915201ac993f4677c61605
i001962@gmail.com <i001962@gmail.com>
2024-04-28 12:37:21 -0700

    closer to perm public url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://xmtp-frames-qxumeiitx-i001962s-projects.vercel.app/";
+    : "https://frames-kmac-guess.vercel.app/";
 
 export const HUB_URL = "hub-grpc.pinata.cloud";
