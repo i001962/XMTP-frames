1a6e4a37634e302aae6f8905d713901c01bf30d6
i001962@gmail.com <i001962@gmail.com>
2024-04-28 15:15:58 -0700

    ad

--- src/app/api/roll/route.ts
+++ src/app/api/roll/route.ts
@@ -35,11 +35,11 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
           <meta name="of:frame:button:1" content="Start over" />
           <meta name="of:frame:button:1:action" content="post" />
-          <meta name="of:frame:button:1:target" content="${postUrl}/api/roll" />
+          <meta name="of:frame:button:1:target" content="${postUrl}" />
       
           <meta name="fc:frame:button:1" content="Start over" />
           <meta name="fc:frame:button:1:action" content="post" />
-          <meta name="fc:frame:button:1:target" content="${postUrl}/api/roll" />
+          <meta name="fc:frame:button:1:target" content="${postUrl}" />
       
           <meta name="of:button:2" content="source code" />
           <meta name="of:button:2:action" content="link" />
