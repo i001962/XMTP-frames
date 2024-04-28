9ce0862b6b8332ef8ffda2c86ceed5a371d1dac6
i001962@gmail.com <i001962@gmail.com>
2024-04-28 12:35:41 -0700

    adds temp vercel public url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://xmtp-frames.vercel.app";
+    : "https://xmtp-frames-qxumeiitx-i001962s-projects.vercel.app/";
 
 export const HUB_URL = "hub-grpc.pinata.cloud";
